9986c369dbcb86e1d5adaf95db1b770583f015ea
Felipe Mendes Neves <felipemen74@gmail.com>
2025-07-20 22:24:43 -0300

    feat: faz as a parte de autenticação de usuario utilizando redux

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,47 +1,10 @@
-import { useForm, type SubmitHandler } from "react-hook-form";
-
-type Inputs = {
-  email: string
-  password: string
-}
+import LoginForm from "../../components/Forms/LoginForm";
 
 const HomePage = () => {
-    const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    } = useForm<Inputs>()
-    const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
     return (
-        <div className="hero bg-base-200 min-h-screen">
-            <div className="hero-content flex-col">
-                <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Entre agora!</h1>
-                    <p className="py-6">
-                        Faça login e tenha acesso ao nosso sistema de agendamento online.
-                    </p>
-                </div>
-                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                    <div className="card-body">
-                        <form onSubmit={handleSubmit(onSubmit)}>
-                            <fieldset className="fieldset">
-                                <label className="label">E-mail</label>
-                                <input type="email" className="input" placeholder="Email" {...register("email", { required: true })} />
-                                {errors.email && <span className="text-red-600">Este campo é obrigatório</span>}
-                                <label className="label">Senha</label>
-                                <input type="password" className="input" placeholder="Senha" {...register("password", { required: true })} />
-                                {errors.password && <span className="text-red-600">Este campo é obrigatório</span>}
-                                <button className="btn btn-neutral mt-4">Login</button>
-                                <div className="flex justify-between">
-                                    <a className="link link-hover">Esqueceu sua senha?</a>
-                                    <a href="/register">Crie sua conta</a>
-                                </div>
-                            </fieldset>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <> 
+            <LoginForm/>
+        </>
     )
 }
 
---
+++ src/store/authSlice.ts
@@ -0,0 +1,59 @@
+import { createSlice, createAsyncThunk, type PayloadAction } from '@reduxjs/toolkit';
+import axios, { AxiosError } from 'axios';
+
+//const api_url = `${import.meta.env.REACT_APP_API_URL}/api/auth`;
+const api_url = "http://localhost:4000/api/auth";
+const user = JSON.parse(localStorage.getItem('token') || '{}');
+
+const initialState = {
+    user: user ? user : null,
+    token: user?.token || null,
+    error: false,
+    success: false,
+    loading: false,
+};
+
+export const fetchUser = createAsyncThunk('user/fetchUser', async (credentials, {rejectWithValue}) => {
+    try {
+        const response = await axios.post(api_url, credentials);
+        console.log(response);
+        localStorage.setItem('token', JSON.stringify(response.data.token.accessToken));
+        return response.data;
+    } catch (error: AxiosError | any) {
+        return rejectWithValue(error.response.data);
+    }   
+});
+
+const authSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        checkToken: (state) => {
+            const user = JSON.parse(localStorage.getItem('token') || '{}');
+            if(user && user.token) {
+                state.user = user;
+                state.token = user.token;
+            } else {
+                state.user = null;
+                state.token = null;
+            }
+        }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchUser.pending, (state) => {
+                state.loading = true;
+                state.error = false;
+            })
+            .addCase(fetchUser.fulfilled, (state, action) => {
+                state.loading = false;
+                state.user = action.payload;
+            })
+            .addCase(fetchUser.rejected, (state, action: PayloadAction<any>) => {
+                state.loading = false;
+                state.error = action.payload.error || "Failed to login";
+            });
+    },
+});
+
+export default authSlice.reducer;
